4a94d9a6c9110aa1270cacd9ee1e34cdac768065
ArtCode101 <sanayut12.service@gmail.com>
2024-02-25 00:44:10 +0700

    Add edit model

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/Lottery.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/Lottery.java
@@ -1,10 +1,16 @@
 package com.kbtg.bootcamp.posttest.lottery;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Data
 @Table(name = "lottery")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Lottery {
 
     @Id
@@ -15,51 +21,9 @@ public class Lottery {
     private String ticket;
 
     @Column(name = "price",nullable = false)
-    private int price;
+    private Integer price;
 
     @Column(name = "amount",nullable = false)
-    private int amount;
-
-    public Lottery() {
-    }
-
-    public Lottery(String id, String ticket, int price, int amount) {
-        this.id = id;
-        this.ticket = ticket;
-        this.price = price;
-        this.amount = amount;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTicket() {
-        return ticket;
-    }
-
-    public void setTicket(String ticket) {
-        this.ticket = ticket;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
+    private Integer amount;
 
 }
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/userTicket/UserTicket.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/userTicket/UserTicket.java
@@ -1,51 +1,24 @@
 package com.kbtg.bootcamp.posttest.userTicket;
 
+import com.kbtg.bootcamp.posttest.lottery.Lottery;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "user_ticket")
 public class UserTicket {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
-    private int id;
+    private Integer id;
 
     @Column(name = "user_id", nullable = false)
     private String user_id;
 
-    @Column(name = "ticket_id",length = 6, nullable = false)
-    private String ticket_id;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "ticket_id",referencedColumnName = "id")
+    private Lottery lottery;
 
-    public UserTicket() {
-    }
-
-    public UserTicket(String user_id, String ticket_id) {
-        this.user_id = user_id;
-        this.ticket_id = ticket_id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getTicket_id() {
-        return ticket_id;
-    }
-
-    public void setTicket_id(String ticket_id) {
-        this.ticket_id = ticket_id;
-    }
 }
