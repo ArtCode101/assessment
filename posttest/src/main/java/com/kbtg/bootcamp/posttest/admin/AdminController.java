615e99dd21fc1f718c30d1ea793d368217da0296
ArtCode101 <sanayut12.service@gmail.com>
2024-02-25 21:48:02 +0700

    fix admin validate and delete autowired

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminController.java
@@ -1,6 +1,6 @@
 package com.kbtg.bootcamp.posttest.admin;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-    @Autowired
     private AdminService adminService;
+
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
 
     @PostMapping("/admin/lotteries")
     public AdminAddTicketResponse addLottery(
-            @RequestBody AdminAddTicketRequest request
+            @Validated @RequestBody AdminAddTicketRequest request
     ){
         return   adminService.addLottery(request);
     }
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminService.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminService.java
@@ -2,16 +2,16 @@ package com.kbtg.bootcamp.posttest.admin;
 
 import com.kbtg.bootcamp.posttest.lottery.Lottery;
 import com.kbtg.bootcamp.posttest.lottery.LotteryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class AdminService {
 
-    @Autowired
     private LotteryRepository lotteryRepository;
+
+    public AdminService(LotteryRepository lotteryRepository){
+        this.lotteryRepository = lotteryRepository;
+    }
 
     public AdminAddTicketResponse addLottery(AdminAddTicketRequest request){
         Lottery lottery = new Lottery(
